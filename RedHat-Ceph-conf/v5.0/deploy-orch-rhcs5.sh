4a1134fa049cc1f9cb7f9c0d49ac409ad162bf20
Alberthua-Perl <hualongfeiyyy@163.com>
2023-10-10 09:40:59 +0800

    Update deploy methods and spec files

--- RedHat-Ceph-conf/v5.0/deploy-orch-rhcs5.sh
+++ RedHat-Ceph-conf/v5.0/deploy-orch-rhcs5.sh
@@ -5,29 +5,40 @@
 # 
 # Method to deploy Ceph Storage Cluster:
 #   - service specification:
-#       - cephadm bootstrap to boot ONE node(mon)
+#       - cephadm bootstrap to boot ONE node
 #       - ceph service specification file
 #   - placement specification:
-#       - cephadm bootstrap to boot ONE node(mon)
+#       - cephadm bootstrap to boot ONE node
 #       - ceph orch host add
 #       - ceph orch host apply
+#       - ceph orch apply
 #       - ceph orch daemon add osd
 #       - ceph orch host add label
 #
 # In this scenario, we use SECOND method to deploy RHCS5.
 
+### set variables
+START="\033[1;37m"
+END="\033[0m"
+TGT_NODE=serverc.lab.example.com
+RGW_SPEC_FILE=/root/rgw-service.yml
+
+### destroy prebuild ceph cluster
+echo -e "\n${START}---> Destroy prebuild ceph cluster...${END}"
+ssh student@workstation 'lab start deploy-deploy'
+
 ### verify deploy node
-echo "---> Verify deploy node if or not serverc..."
-if [[ $(hostname) == 'serverc.lab.example.com' ]]; then
-  echo -e " --> Start deploy RHCS5...\n"
+echo -e "\n${START}---> Verify deploy node if or not serverc...${END}"
+if [[ $(hostname) == ${TGT_NODE} ]]; then
+  echo -e " ${START}--> Start deploy RHCS5...${END}\n"
 else
-  echo " --> NO correct deploy node, EXIT..."
-  echo -e " --> Please run script on serverc node...\n"
+  echo " ${START}--> NO correct deploy node, EXIT...${END}"
+  echo -e " ${START}--> Please run script on serverc node...${END}\n"
   exit 2
 fi
 
 ### install cephadm-ansible package
-echo "---> Install cephadm-ansible and setup cephadm-preflight..."
+echo -e "${START}---> Install cephadm-ansible and setup cephadm-preflight...${END}"
 dnf install -y cephadm-ansible
 cat > /usr/share/cephadm-ansible/hosts <<EOF
 clienta.lab.example.com
@@ -41,7 +52,7 @@ ansible-playbook -i /usr/share/cephadm-ansible/hosts \
 # setup ceph repository customized other than default
 
 ### bootstrap ceph cluster through customized registry
-echo -e "\n---> Bootstrap ceph cluster..."
+echo -e "\n${START}---> Bootstrap ceph cluster...${END}"
 cephadm bootstrap --mon-ip 172.25.250.12 \
 --initial-dashboard-password redhat \
 --dashboard-password-noupdate \
@@ -51,7 +62,7 @@ cephadm bootstrap --mon-ip 172.25.250.12 \
 --registry-password redhat
 
 ### copy ceph.pub for other ceph nodes and add nodes
-echo -e "\n---> Copy nodes ceph.pub and add nodes..."
+echo -e "\n${START}---> Copy nodes ceph.pub and add nodes...${END}"
 ssh-copy-id -f -i /etc/ceph/ceph.pub root@serverd
 ssh-copy-id -f -i /etc/ceph/ceph.pub root@servere
 ssh-copy-id -f -i /etc/ceph/ceph.pub root@clienta
@@ -60,35 +71,50 @@ ceph orch host add servere.lab.example.com
 ceph orch host add clienta.lab.example.com
 
 ### setup ceph monitor service on nodes
-echo -e "\n---> Deploy all ceph monitor service on nodes..."
+echo -e "\n${START}---> Deploy ceph monitor service on nodes...${END}"
 ceph orch apply mon --placement="serverc.lab.example.com serverd.lab.example.com servere.lab.example.com"
 
 ### setup ceph manager service on nodes
-echo -e "\n---> Deploy all ceph manager service on nodes..."
+echo -e "\n${START}---> Deploy ceph manager service on nodes...${END}"
 ceph orch apply mgr --placement="serverc.lab.example.com serverd.lab.example.com servere.lab.example.com"
 
+### setup rados gateway instances on nodes
+echo -e "\n${START}---> Deploy rados gateway instances on nodes...${END}"
+cat > ${RGW_SPEC_FILE} <<EOF
+service_type: rgw
+service_id: realm.zone
+service_name: rgw.realm.zone
+placement:
+  count: 2
+  # each host including one instance
+  hosts:
+    - serverc.lab.example.com
+    - serverd.lab.example.com
+EOF
+ceph orch apply -i ${RGW_SPEC_FILE}
+
 ### setup ceph osds
-echo -e "\n---> Deploy all ceph osds..."
+echo -e "\n${START}---> Deploy ceph osds...${END}"
 for i in {c..e}; do
-  echo " --> Start add OSDs on server${i}..."
-  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdb &>/dev/null
-  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdc &>/dev/null
-  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdd &>/dev/null
-  sleep 5
+  echo -e " ${START}--> Start add OSDs on server${i}...${END}"
+  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdb
+  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdc
+  ceph orch daemon add osd server${i}.lab.example.com:/dev/vdd
+  sleep 3
 done
 
 ### setup ceph cluster admin nodes
-echo -e "\n---> Setup ceph cluster admin nodes..."
+echo -e "\n${START}---> Setup ceph cluster admin nodes...${END}"
 ceph orch host label add serverc.lab.example.com _admin
 ceph orch host label add clienta.lab.example.com _admin
 scp /etc/ceph/ceph.conf /etc/ceph/ceph.client.admin.keyring root@clienta:/etc/ceph
-echo " --> Verify ceph cluster status..."
-cephadm shell ceph status
+echo -e " ${START}--> Verify ceph cluster status...${END}"
+cephadm shell -- ceph status
 # verify ceph cluster status
 
 ### login admin@clienta to verify cluster admin
-echo -e "\n---> Verify ceph cluster status from admin@clienta..."
-echo -e "---> Waiting ceph cluster \033[1;32mHEALTH_OK\033[0m ..."
+echo -e "\n${START}---> Verify ceph cluster status from admin@clienta...${END}"
+echo -e "${START}---> Waiting ceph cluster${END} \033[1;32mHEALTH_OK\033[0m ${START}...${END}"
 # setup interval to wait ceph cluster to become health,
 # If cluster still report HEALTH_WARN without any other messages, 
 # please wait until HEALTH_OK. 
@@ -97,7 +123,7 @@ PID=$!
 FRAMES="/ | \\ -"
 while [[ -d /proc/$PID ]]; do
   for FRAME in $FRAMES; do
-    printf "\r%s Waiting..." "$FRAME"  # Do NOT use variables in printf command
+    printf "\r%s ${START}Waiting...${END}" "$FRAME"  # DO NOT use variables in printf
     sleep 0.5
   done
 done; printf "\n"
